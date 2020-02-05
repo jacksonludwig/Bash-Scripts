b70e0ab62eddadb4dda05378cf7ed3b8604951d9
Jackson Ludwig <ludwj61@mail.sunysuffolk.edu>
2020-02-04 22:16:00 -0500

    raspberry pi auto-commit

--- autogitwork.sh
+++ autogitwork.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(cd ~/DiscordBot &) && (git pull &) && (pkill -f "node" &) && (git add . &) && (git commit -m "raspberry pi auto-commit" &) && (git push &)
+(cd ~/DiscordBot &) && (git pull &) && (sleep 60 &) && (pkill -f "node" &) && (git add . &) && (git commit -m "raspberry pi auto-commit" &) && (git push &)
