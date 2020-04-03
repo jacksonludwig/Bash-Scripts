41d00f30d3e55198c8c09e6b805087e7ffcd117a
Jackson Ludwig <ludwj61@mail.sunysuffolk.edu>
2020-04-03 14:40:05 -0400

    add python

--- autorestart.sh
+++ autorestart.sh
@@ -4,5 +4,10 @@ do
 	if ! pgrep -x "node" > /dev/null
 	then
 		(cd ~/DiscordBot && node .) & disown
-	fi	
+	fi
+
+	if ! pgrep -x "python3" > /dev/null
+	then
+		(cd ~/SmiteBot && python3 bot.py) & disown
+	fi
 done
